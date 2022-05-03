349f358c2377175145dcc5326e2f4e8b2eec6fea
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2022-05-03 09:52:54 -0400

    Add workflow

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:lts-bullseye
-
+LABEL IMAGE_SOURCE="https://github.com/codefresh-contrib/github-users-sync.git"
 ENV GH_TEAM_PERMISSIONS="admin"
 ENV CF_SUPERADMIN_EMIAL="admin@local"
 
--- config/config.js
+++ config/config.js
@@ -1,9 +1,14 @@
 module.exports = {
+    // Don't run user modification operations
     DRY_RUN: process.env.DRY_RUN === "true",
+    // Display debug messages
     DEBUG: process.env.DEBUG === "true",
+    // Don't delete existing users, only add new users
     NO_DELETE: process.env.NO_DELETE === "true",
+    // Codefresh configuration
     codefresh: {
         apiKey: process.env.CF_API_KEY,
+        // Codefresh account id #
         accountId: process.env.CF_ACCOUNT_ID,
         // user's email to retain even if they are not on any github org/team (will not add this user, only prevent it from being removed)
         superadmin: process.env.CF_SUPERADMIN_EMIAL || "admin@local"
@@ -11,6 +16,7 @@ module.exports = {
     github: {
         accessToken: process.env.GH_API_KEY
     },
+    // GitHub organization(s) information to sync with
     organizations: [
         {
             name: process.env.GH_ORG_NAME,
